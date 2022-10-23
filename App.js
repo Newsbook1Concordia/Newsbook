c4d7df93d5c074f0f8ef7bff05da7c51170f3a67
Vishruth Khatri <itsvishruthkhatri@hotmail.com>
2022-10-23 17:04:49 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import {useEffect} from "react";
+import jwt_Decode from "jwt-decode";
+import './App.css';
+
+
+
+function App() {
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token : " + response.credential);
+    var userObject = jwt_Decode(response.credential);
+    console.log(userObject);
+  }
+
+  useEffect(() => {
+  /*global google*/
+    google.accounts.id.initialize({
+      client_id : '138640372450-e85pkrmnflipgal3uf7rosjok6vc6fov.apps.googleusercontent.com',
+      callback : handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        {theme:"outline",size:"large"}
+    );
+    },[]);
+  return (
+    <div className="App">
+      <div id = "signInDiv"></div>
+    </div>
+  );
+}
+export default App;
