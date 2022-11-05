b875df106f589e7ed28b4c0ac231492f17ab0806
ashishshukla08 <116211738+ashishshukla08@users.noreply.github.com>
2022-11-05 16:23:07 -0400

    Add files via upload

--- components/Googlebutton.js
+++ components/Googlebutton.js
@@ -1,17 +1,20 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import jwt_Decode from "jwt-decode";
 
-
-
-
-export default function GoogleLoginnew() {
+export default function G_login() {
+  const [ user , setUser] = useState({});
 
   function handleCallbackResponse(response) {
     console.log("Encoded JWT ID token : " + response.credential);
     var userObject = jwt_Decode(response.credential);
     console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
   }
-
+  function handleSignOut(){
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
   useEffect(() => {
   /*global google*/
     google.accounts.id.initialize({
@@ -23,10 +26,27 @@ export default function GoogleLoginnew() {
         document.getElementById("signInDiv"),
         {theme:"outline",size:"large"}
     );
+
+    google.accounts.id.prompt();
     },[]);
+  
   return (
-    <div className="App">
+    
+    <div className="Google">
+
       <div id = "signInDiv"></div>
+      {
+        Object.keys(user).length != 0 &&
+        <button onClick = {(e) => handleSignOut(e)}>Sign Out</button>
+      }
+
+      { user &&
+      <div>
+        <img src={user.picture}></img>
+        <h3>{user.name}</h3>
+      </div>
+      }
     </div>
+    
   );
 }
