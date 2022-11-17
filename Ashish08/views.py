1ad28b54f0d8156a409bf3d7b09a15e47990a3b2
ashishshukla08 <116211738+ashishshukla08@users.noreply.github.com>
2022-11-17 02:06:03 -0500

    Add files via upload

--- Ashish08/views.py
+++ Ashish08/views.py
@@ -4,43 +4,28 @@ import uuid
 import requests
 import allauth
 import requests
-from allauth.account.models import EmailAddress
 from Lib.db import User
 from django.shortcuts import render
-
 from google.oauth2 import id_token
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 import google.auth.transport.requests
-
-API_KEY = '90237a5a846744c38ba89566f26af087'
-
-
 from sqlalchemy import Column, Integer,String,ForeignKey,INTEGER,Float
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base,relationship
 from sqlalchemy.orm import sessionmaker
 import uuid
-
 import os
 from sqlalchemy import create_engine, MetaData,Table,Column,String,ForeignKey,INTEGER,Float
 from sqlalchemy.dialects.postgresql import UUID
-
-
+API_KEY = '90237a5a846744c38ba89566f26af087'
 Base = declarative_base()
 
-#   User Table
-
-
 # Create your views here.
-os.environ["DATABASE_URL"] = "cockroachdb://ashish:O6dEEC2SpIwwfc03nwsOZQ@free-tier11.gcp-us-east1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dlair-python-2718"
+os.environ["DATABASE_URL"] = "cockroachdb://ashish:ltb1Pj_BqF4Yp0exVkfuWA@free-tier11.gcp-us-east1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dlair-python-2718"
 engine = create_engine(os.environ["DATABASE_URL"])
-
-
 Session=sessionmaker(bind=engine)
 session=Session()
-
 def update_country(session,email,country):
     req=session.query(User).filter(User.Email == email).first()
     req.Country=country
@@ -54,33 +39,31 @@ class User(Base):
     
     # category=relationship("Category")
 
-
-
-########## Add User To Table
-def add_user(name,email,country):
-    user_id = uuid.uuid4()
-    new_user = User(id=user_id,Email=email,Name=name,Country=country)
-    session.add(new_user)
+def addUser(email,name,country):
     
-############ Get User Information From User Email
-def get_user_info(email):
+    user_id = uuid.uuid4()
+    new_user=User(id=user_id, Email=email,Name=name,Country=country)
+    session.add(new_user)
+    return user_id
+
+def get_user_info(email):                                                   ############ Get User Information From User Email
     user:User=session.query(User).filter(User.Email == email).first()
     return user
-
-
 def login(request):
-  
-    
-    return render(request,'login.html',{})
+  return render(request,'login.html',{})
 
 def news(request):
-    
+    email=request.user.email
+   
     if request.method == 'POST' :
-        print(".......................")
+        print("............country...........")
         country = request.POST.get('country')
-        
-        
-        
+        print(country)
+        update_country(session,email,country)
+        print('.........country updated in database')
+    
+    
+       
     url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
     response = requests.get(url)
     data = response.json()
@@ -88,29 +71,33 @@ def news(request):
     if request.method == 'POST' :   
         check = request.POST.getlist('category')
         print(check)
-    
     context = {
         'articles' : articles,
         'category' : check,
         'country'  : country,
         
     }
-
     return render(request,'news.html',context)
 
-
-
-def category(request):
+def category(request):                      ######After login Category selection
     # name = get.EmailAddress
-    
-    name=request.user
-    name_user = str(name)
+    ema=request.user.email
+    email=str(ema)
+    username=str(request.user)
+    default ='ca'
+    print(default)
+    addUser(email,username,default)
+    print('newprint')
     print('............')
-    print(name_user)
+    print(username)
+    print(email)
     
-    email=request.user.email
+    print('...........get user......')
+    a= (get_user_info(email))
+    print(a.Country)
     
-    # add_user(name_user,email,country)
+    
+    
     # add_user(name,email)
     # email='kartik.faldu1207@gmail.com'
     # user = get_user_info(email)
@@ -131,17 +118,19 @@ def category(request):
     
 #     return render (request,"countrycode-container")
 
-    
-def profile(request):
+   
+def profile(request):                ####### User Profile
     return render(request,'profile.html',{})
 
-def business(request):  
-    email=User.Email
-    cat= get_user_info(User.Email)
+def business(request): #########1st Category wise
     
+    cat= get_user_info(User.Email)
+    print('.................Business.............')
+    
+    # email=request.user.email
     print(cat)
     print('..........country............')
-    country=User.Country
+    country=cat.Country
     print(country)
     
    
@@ -155,7 +144,6 @@ def business(request):
     data = response.json()
     articles = data['articles']
 
-    
     context = {
         'articles' : articles,
         # 'category' : check,
